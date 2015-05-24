d0e42338c80221b4edfce678a931f6c4cc25ccfa
Ryan M <rjames86@gmail.com>
2015-05-24 14:26:03 -0700

    testing config

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from config import config
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
+    print "CONFIG!!", app.config
     config[config_name].init_app(app)
 
     from controllers.main import main as main_blueprint
